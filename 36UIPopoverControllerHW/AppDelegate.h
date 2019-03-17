d69cb88b4c81b208f8e137908c3469c1f3d445ac
Eduard Galchenko <eddiegalchenko@gmail.com>
2019-03-17 18:35:09 +0200

    Initial Commit

---
+++ 36UIPopoverControllerHW/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  36UIPopoverControllerHW
+//
+//  Created by Eduard Galchenko on 3/17/19.
+//  Copyright © 2019 Eduard Galchenko. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ 36UIPopoverControllerHW/ViewController.m
@@ -0,0 +1,23 @@
+//
+//  ViewController.m
+//  36UIPopoverControllerHW
+//
+//  Created by Eduard Galchenko on 3/17/19.
+//  Copyright © 2019 Eduard Galchenko. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+@end
